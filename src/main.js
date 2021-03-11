be9aae6f66ce6bc4b8e81e5d36fca580b9f9dc1a
Maria-7514 <evangeline73.mjhe@gmail.com>
2021-03-11 19:58:41 +0000

    card generator

--- src/main.js
+++ src/main.js
@@ -2,9 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   console.log("Hello Rigo from the console!");
 };
+var arr = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let element = Math.floor(Math.random() * arr.length);
+let number = arr[element];
+
+let change = document.getElementById("numero");
+change.innerHTML = number;
+
+let symbol = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+let symbolform = Math.floor(Math.random() * symbol.length);
+let symbolElem = symbol[symbolform];
+
+let change2 = document.getElementById("simbolo");
+change2.innerHTML = symbolElem;
+
+let change3 = document.getElementById("symbolbtn");
+change3.innerHTML = symbolElem;
+
+// //if (symbolform == "&spades;" || symbolform == "&clubs;") {
+//   return (symbolElem.color = "black");
+// } else {
+//   return (symbolElem.color = "red");
+// }//
+if (symbolElem === "&hearts;" || symbolElem === "&diams;") {
+  document.getElementById("simbolo").classList.add("cardColorRed");
+  document.getElementById("symbolbtn").classList.add("cardColorRed");
+}
